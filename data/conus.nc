d1f0ee444defb0afe9c23c5cbaf14f5d8210179f
David Lafferty <dcl257@cornell.edu>
2024-09-06 17:04:54 -0400

    Added a simple spatial run

---
+++ data/conus.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e18dfb51ad8753143f4aa4b97ebe49fab0ac1050d69a49931dc2edd97e5f5a86
+size 243760064
--- data/data_wrangling.py
+++ data/data_wrangling.py
@@ -1,6 +1,9 @@
 import numpy as np
 import xarray as xr
 
+###################
+# MCLEAN
+###################
 # McLean county coordinates
 lat, lon = 40.61752192312278, -88.68366477345201
 
@@ -37,3 +40,43 @@ lai = npz["lai"][ix, iy, :]
 np.savetxt("./data/mclean_county_daily_tas_degC_2016-2022.csv", tas)
 np.savetxt("./data/mclean_county_daily_prcp_mm_2016-2022.csv", prcp)
 np.savetxt("./data/mclean_county_daily_lai_climatology.csv", lai)
+
+
+###############
+# GMFD
+###############
+# Read all climate data
+tas = xr.open_mfdataset("../data/gmfd/tas_*.nc", combine="by_coords")
+prcp = xr.open_mfdataset("../data/gmfd/prcp_*.nc", combine="by_coords")
+
+
+# Subset to US
+def subset_conus(ds):
+    import regionmask
+
+    countries = regionmask.defined_regions.natural_earth_v5_0_0.countries_50
+    mask = countries.mask(tas.longitude, tas.latitude)
+    mask_conus = mask == countries.map_keys("United States of America")
+
+    ds_conus = ds.where(mask_conus, drop=True)
+    ds_conus = ds_conus.where(
+        (ds_conus.latitude <= 50) & (ds_conus.longitude > 230) & (ds_conus.latitude > 20),
+        drop=True,
+    )  # drop AL, HI
+
+    return ds_conus
+
+
+# Do it
+tas_conus = subset_conus(tas)
+prcp_conus = subset_conus(prcp)
+
+# Store
+ds = xr.merge(
+    [
+        tas_conus.convert_calendar(calendar="noleap", dim="time").isel(z=0).drop_vars("z"),
+        prcp_conus.convert_calendar(calendar="noleap", dim="time").isel(z=0).drop_vars("z"),
+    ]
+)
+
+ds.to_netcdf("../data/gmfd/conus.nc")
